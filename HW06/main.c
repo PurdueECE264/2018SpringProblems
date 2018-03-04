96319be4b8411fd19d73fe13d19caa0ee36051b2
wumenghan <wu784@purdue.edu>
2018-03-04 16:40:21 -0500

    Update main.c

--- HW06/main.c
+++ HW06/main.c
@@ -5,11 +5,13 @@
 /*
 	Complete the main function and meet the following specifications.
 	In this main function you will
-	1. Use `Connect()` function to connect to database.txt. You should hard code `database.txt` in `Connect()` function.
-	2. Pass `argc`, `argv` to `ParseQuery()` and get the parse results.
-	3. Call `ExecuteQuery()` function to get the result of students who meet the condition we specify in the query. 
-	4. Use `WriteDb()` to write to file named `output.txt`. You should hard code this name. 
-	5. Free memory.
+	1. `argv[1]`: database name (i.e., database.txt). You should pass `argv[1]` to `Connect()`.
+	2. `argv[2]`: output file name. You should pass `argv[2]` to `WriteDb()`.
+	3. Pass `argc`, `argv` to `ParseQuery()` and get the parse results.
+	4. Call `ExecuteQuery()` function to get the result of students who meet the condition we specify in the query.
+	5. Use `WriteDb()` to write a file.
+	6. Free memory.
+	7. You can assume all input arguments are valid. 
  */
 int main(int argc, char **argv) {
     return EXIT_SUCCESS;
